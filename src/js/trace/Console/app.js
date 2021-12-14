dcda8e16ac8acab471235427216486cb26a60f3c
Juliano Costa <julianocosta89@outlook.com>
2021-12-14 22:39:51 +0100

    Added console-app single file

---
+++ src/js/trace/Console/app.js
@@ -0,0 +1,30 @@
+const api = require ('@opentelemetry/api');
+const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+// Creates the tracer provider and configures OTLP collector
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'nodejs.console.app',
+  }),
+});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()));
+
+provider.register();
+
+// Creates the tracer
+const tracer = api.trace.getTracer("nodejs.console.app");
+
+// Start a span with an attribute
+const span = tracer.startSpan("HelloWorldSpan", { 
+  attributes: { 
+    foo : 'bar' 
+  } 
+});
+
+console.log('Hello World');
+
+span.end();
