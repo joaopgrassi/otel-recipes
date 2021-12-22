9e074ffa2844ac91c59f580b062ded75c296a40b
Juliano Costa <julianocosta89@outlook.com>
2021-12-22 23:20:00 +0100

    Added suggestions

--- src/java/trace/console/app/src/main/java/otel/recipes/App.java
+++ src/java/trace/console/app/src/main/java/otel/recipes/App.java
@@ -1,36 +1,35 @@
 package otel.recipes;
 
-import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
-
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
 public class App {
-    public static void main(String[] args) {
-        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().build();
+  public static void main(String[] args) {
+    OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().build();
 
-        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-            .setTracerProvider(
-                SdkTracerProvider.builder()
-                    .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))
-                    .setResource(
-                        Resource.create(
-                            Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
-                    .build())
-            .build();
+    OpenTelemetrySdk.builder()
+        .setTracerProvider(
+            SdkTracerProvider.builder()
+                .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))
+                .setResource(
+                    Resource.create(
+                        Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
+                .build())
+        .buildAndRegisterGlobal();
 
-        Tracer tracer = openTelemetry.getTracer("java.console.app");
+    Tracer tracer = GlobalOpenTelemetry.getTracer("java.console.app");
 
-        Span span = tracer.spanBuilder("HelloWorldSpan").startSpan();
-        span.setAttribute("foo", "bar");
-        span.end();
-        System.out.println("Hello World");
-    }
+    Span span = tracer.spanBuilder("HelloWorldSpan").startSpan();
+    span.setAttribute("foo", "bar");
+    span.end();
+    System.out.println("Hello World");
+  }
 }
