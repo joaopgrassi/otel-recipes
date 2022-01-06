12919a8ad191e9f52c8bfbc9a06c6ed27ca270b1
Juliano Costa <julianocosta89@outlook.com>
2022-01-06 20:54:40 +0100

    Added Java CI test

--- src/java/trace/console/app/src/main/java/otel/recipes/App.java
+++ src/java/trace/console/app/src/main/java/otel/recipes/App.java
@@ -15,21 +15,26 @@ public class App {
   public static void main(String[] args) {
     OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().build();
 
-    OpenTelemetrySdk.builder()
-        .setTracerProvider(
-            SdkTracerProvider.builder()
-                .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))
-                .setResource(
-                    Resource.create(
-                        Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
-                .build())
-        .buildAndRegisterGlobal();
+    // Builds the Tracer Provider with span processor and resource attribute
+    SdkTracerProvider tracerProvider =
+        SdkTracerProvider.builder()
+            .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))
+            .setResource(
+                Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
+            .build();
 
+    // Sets and registers the Tracer Provider as Global
+    OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).buildAndRegisterGlobal();
+
+    // Creates the tracer
     Tracer tracer = GlobalOpenTelemetry.getTracer("java.console.app");
 
+    // Creates a span, set its attributes and finishes it
     Span span = tracer.spanBuilder("HelloWorldSpan").startSpan();
     span.setAttribute("foo", "bar");
     span.end();
-    System.out.println("Hello World");
+
+    // Shuts the Trace Provider down after finishing its usage
+    tracerProvider.shutdown();
   }
 }
