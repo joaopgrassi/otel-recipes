918b1d5ea67ddaa1a065ee451e109209cd04cf2a
Joao Grassi <joao@joaograssi.com>
2022-01-08 17:00:46 +0100

    Add test for JSON Schema validation

---
+++ test/jsonschema/go.mod
@@ -0,0 +1,11 @@
+module github.com/joaopgrassi/otel-recipes/test/jsonschema
+
+go 1.17
+
+require github.com/xeipuuv/gojsonschema v1.2.0
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+)
---
+++ test/jsonschema/jsonschema_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/xeipuuv/gojsonschema"
+)
+
+var lang = flag.String("lang", "none", "The name of the language folder containing the recipefile.json to be validated")
+
+func TestJsonSchema(t *testing.T) {
+	cwd, _ := os.Getwd()
+	root := filepath.Clean(filepath.Join(cwd, "..", ".."))
+
+	schemaPath := "file://" + filepath.Join(root, "otel-recipes-schema.json")
+	recipeFile := "file://" + filepath.Join(root, "src", *lang, "recipefile.json")
+
+	t.Logf("The schema path to load is: %s", schemaPath)
+	t.Logf("The recipefile path to load is: %s", recipeFile)
+
+	schemaLoader := gojsonschema.NewReferenceLoader(schemaPath)
+	documentLoader := gojsonschema.NewReferenceLoader(recipeFile)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		t.Fatalf("Failed validating the JSON schema: %v", err)
+	}
+
+	if !result.Valid() {
+		t.Errorf("The JSON schema is not valid. See errors:")
+		for _, desc := range result.Errors() {
+			t.Logf("- %s", desc)
+		}
+	}
+}
