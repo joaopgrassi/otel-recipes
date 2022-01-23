1670ac7a885a932791c43607b0b8984370d7d69f
Joao Grassi <joao@joaograssi.com>
2022-01-23 15:26:02 +0100

    Add header menu items

--- src/site/src/lib/header/Header.svelte
+++ src/site/src/lib/header/Header.svelte
@@ -1,23 +1,67 @@
 <script lang="ts">
 	import logo from './svelte-logo.svg';
+	import { page } from '$app/stores';
+	let showNav = false;
 </script>
 
-<nav class="navbar is-relative is-transparent py-3 has-background-light">
+<nav class="navbar py-3 site-nav">
 	<div class="container">
 		<div class="navbar-brand">
 			<a class="navbar-item" href="#">
 				<img src={logo} alt="" style="min-height: 40px;" width="auto" />
 			</a>
+			<a
+				role="button"
+				class="navbar-burger"
+				aria-label="menu"
+				aria-expanded="false"
+				data-target="navMenu"
+				class:is-active={showNav}
+				on:click={(e) => (showNav = !showNav)}
+			>
+				<span aria-hidden="true" />
+				<span aria-hidden="true" />
+				<span aria-hidden="true" />
+			</a>
 		</div>
-		<div class="navbar-menu">
+		<div class="navbar-menu" class:is-active={showNav} id="navMenu">
 			<div class="navbar-end">
-				<div class="navbar-item">
-					<div class="buttons">
-						<a class="button is-primary is-outlined" href="#">Log In</a>
-						<a class="button is-primary" href="#">Sign Up</a>
-					</div>
-				</div>
+				<a href="/" class="navbar-item" class:is-active={$page.url.pathname === '/'}>Home</a>
+				<a href="/recipes" class="navbar-item" class:is-active={$page.url.pathname === '/recipes'}>
+					Recipes
+				</a>
 			</div>
 		</div>
 	</div>
 </nav>
+
+<style lang="scss">
+	@import 'bulma/sass/utilities/initial-variables.sass';
+
+	.site-nav {
+		background-color: $white-ter;
+	}
+
+	.navbar-menu .navbar-item {
+		margin-left: 1.2rem;
+		font-size: 1.3rem;
+		padding: 0;
+	}
+
+	a.navbar-item.is-active:not(:focus):not(:hover),
+	.navbar-link.is-active:not(:focus):not(:hover) {
+		color: $grey-dark;
+	}
+
+	a.navbar-item:focus,
+	a.navbar-item:focus-within,
+	a.navbar-item:hover,
+	a.navbar-item.is-active,
+	.navbar-link:focus,
+	.navbar-link:focus-within,
+	.navbar-link:hover,
+	.navbar-link.is-active {
+		background-color: transparent;
+		color: $orange;
+	}
+</style>
