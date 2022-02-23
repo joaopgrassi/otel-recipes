287722c6f11f49683ba4378d6a3e4f4378148a64
Joao Grassi <joao@joaograssi.com>
2022-02-23 23:47:38 +0100

    Use static svg instead of in code

--- src/site/src/routes/index.svelte
+++ src/site/src/routes/index.svelte
@@ -113,13 +113,7 @@
 		<div class="columns is-5 is-variable is-vcentered">
 			<div class="column is-half">
 				<div class="browser-mockup">
-					<svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
-						<g fill="none" fill-rule="evenodd" transform="translate(1 1)">
-							<circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5" />
-							<circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5" />
-							<circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5" />
-						</g>
-					</svg>
+					<img src="browser-buttons.svg" alt="browser top-bar icons" />
 					<div class="container mt-2">
 						{#key code}
 							<pre in:fade>{code}</pre>
