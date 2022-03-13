c289aac96d7c91239035ed81bc72a6a3751e091b
Joao Grassi <joao@joaograssi.com>
2022-03-13 19:46:37 +0100

    Fix some left overs from svelte template

--- src/site/src/routes/recipes/index.svelte
+++ src/site/src/routes/recipes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { fly } from 'svelte/transition';
