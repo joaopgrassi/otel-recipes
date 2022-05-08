54e39b68d525fc9dfa310777c89eaa28fb97be04
Joao Grassi <joao@joaograssi.com>
2022-05-08 12:15:53 +0200

    Add some comments

--- src/csharp/trace/aspnet-api/Controllers/HelloWorldController.cs
+++ src/csharp/trace/aspnet-api/Controllers/HelloWorldController.cs
@@ -9,8 +9,10 @@ public class HelloWorldController : Controller
     [HttpGet]
     public IActionResult Get()
     {
+        // Start a span with a tag
         using var activity = Startup.Tracer.StartActivity("HelloWorldSpan");
         activity?.SetTag("foo", "bar");
+
         return Ok("Hello world!");
     }
 }
