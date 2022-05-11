7d9b23f6d023170eb232a81b01d878e627a3fb0f
Joao Grassi <joao@joaograssi.com>
2022-05-11 23:22:37 +0200

    Add a Gin API trace sample

---
+++ src/go/trace/gin-api/app.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
+	"log"
+)
+
+const serviceName = "go.gin.api"
+
+// Creates the tracer to be shared across the application
+var tracer = otel.Tracer(serviceName)
+
+func main() {
+	// Configures the SDK and later the Gin instrumentation (middleware)
+	// Exports to a locally running collector on port 4317
+	tp := initTracer()
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	r := gin.New()
+	r.Use(otelgin.Middleware(serviceName))
+	r.GET("/helloworld", func(c *gin.Context) {
+
+		// Start a span with an attribute
+		_, span := tracer.Start(
+			c.Request.Context(),
+			"HelloWorldSpan",
+			trace.WithAttributes(attribute.String("foo", "bar")))
+		defer span.End()
+
+		c.String(200, "Hello world!")
+	})
+	_ = r.Run(":8080")
+}
+
+func initTracer() *sdktrace.TracerProvider {
+	ctx := context.Background()
+
+	// Creates a resource with the service.name attribute
+	res, err := resource.New(ctx,
+		resource.WithAttributes(semconv.ServiceNameKey.String(serviceName)),
+	)
+	handleErr(err, "failed to create the resource")
+
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithInsecure())
+	handleErr(err, "failed to create the trace exporter")
+
+	// Configures the SDK, exporting to a local running Collector
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(sdktrace.NewSimpleSpanProcessor(traceExporter)),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
+
+func handleErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %v", message, err)
+	}
+}
