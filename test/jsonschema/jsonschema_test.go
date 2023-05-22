84d81371c7febddce61f2f3c57fcfe9c22477a84
Joao Grassi <git@joaograssi.com>
2023-05-22 22:26:37 +0200

    Adapt tests to consider recipefiles in other folders (#80)

--- test/jsonschema/jsonschema_test.go
+++ test/jsonschema/jsonschema_test.go
@@ -9,30 +9,40 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-var language = flag.String("language", "none", "The name of the language folder containing the recipefile.json to be validated")
+var lang = flag.String("lang", "none", "The name of the lang folder containing the recipefile.json to be validated")
+
+var telemetrySignals = []string{"trace", "metrics", "logs"}
 
 func TestJsonSchema(t *testing.T) {
 	cwd, _ := os.Getwd()
 	root := filepath.Clean(filepath.Join(cwd, "..", ".."))
 
-	schemaPath := "file://" + filepath.Join(root, "otel-recipes-schema.json")
-	recipeFile := "file://" + filepath.Join(root, "src", *language, "recipefile.json")
+	for _, ts := range telemetrySignals {
+		sp := filepath.Join(root, "src", *lang, ts)
+		if _, err := os.Stat(sp); os.IsNotExist(err) {
+			t.Logf("No recipes yet for telemetry signal: %s", ts)
+			continue
+		}
 
-	t.Logf("The schema path to load is: %s", schemaPath)
-	t.Logf("The recipefile path to load is: %s", recipeFile)
+		schemaPath := "file://" + filepath.Join(root, "otel-recipes-schema.json")
+		recipeFile := "file://" + filepath.Join(sp, "recipefile.json")
 
-	schemaLoader := gojsonschema.NewReferenceLoader(schemaPath)
-	documentLoader := gojsonschema.NewReferenceLoader(recipeFile)
+		t.Logf("The schema path to load is: %s", schemaPath)
+		t.Logf("The recipefile path to load is: %s", recipeFile)
 
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
-	if err != nil {
-		t.Fatalf("Failed validating the JSON schema: %v", err)
-	}
+		schemaLoader := gojsonschema.NewReferenceLoader(schemaPath)
+		documentLoader := gojsonschema.NewReferenceLoader(recipeFile)
 
-	if !result.Valid() {
-		t.Errorf("The JSON schema is not valid. See errors:")
-		for _, desc := range result.Errors() {
-			t.Logf("- %s", desc)
+		result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+		if err != nil {
+			t.Fatalf("Failed validating the JSON schema: %v", err)
+		}
+
+		if !result.Valid() {
+			t.Errorf("The JSON schema is not valid. See errors:")
+			for _, desc := range result.Errors() {
+				t.Logf("- %s", desc)
+			}
 		}
 	}
 }
