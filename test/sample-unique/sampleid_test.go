237ec23dbda62c0bdd582d591eb538742adac3d3
Joao Grassi <git@joaograssi.com>
2023-05-22 22:24:08 +0200

    Adapt tests to consider recipefiles in other folders

--- test/sample-unique/sampleid_test.go
+++ test/sample-unique/sampleid_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"io/ioutil"
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
@@ -23,29 +23,38 @@ type Sample struct {
 	Id string
 }
 
-var language = flag.String("language", "none", "The name of the language folder containing the recipefile.json to be validated")
+var lang = flag.String("lang", "none", "The name of the lang folder")
+
+var telemetrySignals = []string{"trace", "metrics", "logs"}
 
 func TestSampleIdUnique(t *testing.T) {
 	cwd, _ := os.Getwd()
 	root := filepath.Clean(filepath.Join(cwd, "..", ".."))
 
-	fp := filepath.Join(root, "src", *language, "recipefile.json")
+	for _, ts := range telemetrySignals {
+		sp := filepath.Join(root, "src", *lang, ts)
+		if _, err := os.Stat(sp); os.IsNotExist(err) {
+			t.Logf("No recipes yet for telemetry signal: %s", ts)
+			continue
+		}
 
-	t.Logf("The recipefile path to load is: %s", fp)
+		fp := filepath.Join(sp, "recipefile.json")
+		t.Logf("The recipefile path to load is: %s", fp)
 
-	jsonFile, err := os.Open(fp)
-	if err != nil {
-		t.Fatalf("Failed opening recipe json file: %v", err)
-	}
-	defer jsonFile.Close()
+		jsonFile, err := os.Open(fp)
+		if err != nil {
+			t.Fatalf("Failed opening recipe json file: %v", err)
+		}
+		defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var recipe Recipe
-	json.Unmarshal(byteValue, &recipe)
+		byteValue, _ := io.ReadAll(jsonFile)
+		var recipe Recipe
+		json.Unmarshal(byteValue, &recipe)
 
-	for _, s := range recipe.Signals {
-		if hasDuplicatedSamples(s.Samples, t) {
-			t.Errorf("Signal %s has duplicated sample ids. Check the logs to find the culprit", s.Id)
+		for _, s := range recipe.Signals {
+			if hasDuplicatedSamples(s.Samples, t) {
+				t.Errorf("Signal %s has duplicated sample ids. Check the logs to find the culprit", s.Id)
+			}
 		}
 	}
 }
