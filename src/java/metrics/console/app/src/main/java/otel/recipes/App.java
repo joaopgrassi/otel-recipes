424abc0397086075ef1838d0a9869a5277649baa
Joao Grassi <git@joaograssi.com>
2023-05-25 22:45:43 +0200

    Fix linter

--- src/java/metrics/console/app/src/main/java/otel/recipes/App.java
+++ src/java/metrics/console/app/src/main/java/otel/recipes/App.java
@@ -1,5 +1,7 @@
 package otel.recipes;
 
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
+
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.LongCounter;
@@ -11,44 +13,39 @@ import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
-
 public class App {
 
-    public static void main(String[] args) {
-        OtlpGrpcMetricExporter metricsExporter = OtlpGrpcMetricExporter.builder().build();
+  public static void main(String[] args) {
+    OtlpGrpcMetricExporter metricsExporter = OtlpGrpcMetricExporter.builder().build();
 
-        // Builds the Meter Provider with the OTLP exporter
-        SdkMeterProvider meterProvider =
-                SdkMeterProvider.builder()
-                        .setResource(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
-                        .registerMetricReader(
-                                PeriodicMetricReader.builder(metricsExporter).build()
-                        )
-                        .build();
+    // Builds the Meter Provider with the OTLP exporter
+    SdkMeterProvider meterProvider =
+        SdkMeterProvider.builder()
+            .setResource(
+                Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "java.console.app")))
+            .registerMetricReader(PeriodicMetricReader.builder(metricsExporter).build())
+            .build();
 
-        // Sets and registers the Meter Provider as Global
-        OpenTelemetrySdk.builder().setMeterProvider(meterProvider).buildAndRegisterGlobal();
+    // Sets and registers the Meter Provider as Global
+    OpenTelemetrySdk.builder().setMeterProvider(meterProvider).buildAndRegisterGlobal();
 
-        // Creates the meter
-        Meter meter = GlobalOpenTelemetry.getMeter("java.console.app");
+    // Creates the meter
+    Meter meter = GlobalOpenTelemetry.getMeter("java.console.app");
 
-        // Creates the Counter instrument
-        LongCounter counter = meter
-                .counterBuilder("myCounter")
-                .setDescription("I count things!")
-                .setUnit("1")
-                .build();
+    // Creates the Counter instrument
+    LongCounter counter =
+        meter.counterBuilder("myCounter").setDescription("I count things!").setUnit("1").build();
 
-        // Add to our counter with an attribute
-        counter.add(3, Attributes.of(stringKey("foo"), "bar"));
+    // Add to our counter with an attribute
+    counter.add(3, Attributes.of(stringKey("foo"), "bar"));
 
-        // Creates the Gauge instrument passing the callback that will produce the metric values
-        meter.gaugeBuilder("myGauge")
-                .setDescription("I gauge things")
-                .setUnit("1")
-                .buildWithCallback(gauge -> gauge.record(3.5, Attributes.of(stringKey("foo"), "bar")));
+    // Creates the Gauge instrument passing the callback that will produce the metric values
+    meter
+        .gaugeBuilder("myGauge")
+        .setDescription("I gauge things")
+        .setUnit("1")
+        .buildWithCallback(gauge -> gauge.record(3.5, Attributes.of(stringKey("foo"), "bar")));
 
-        meterProvider.shutdown();
-    }
+    meterProvider.shutdown();
+  }
 }
