cd2a5d67a8530497f43ab55bdc8f110a7bb49438
Joao Grassi <joao@joaograssi.com>
2024-01-05 16:58:00 +0100

    Add Spring Boot REST Api sample

---
+++ src/java/trace/springboot-api/src/test/java/com/otel/recipes/springbootapi/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otel.recipes.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
