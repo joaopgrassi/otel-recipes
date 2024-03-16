ad338f9dd28305642e1fecf5064584ccfd25b575
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-03-16 19:03:31 +0100

    Make trace utils call otlp server

--- internal/common/go.mod
+++ internal/common/go.mod
@@ -2,4 +2,6 @@ module github.com/joaopgrassi/otel-recipes/internal/common
 
 go 1.22.1
 
-require go.opentelemetry.io/proto/otlp v1.1.0 // indirect
+require go.opentelemetry.io/proto/otlp v1.1.0
+
+require google.golang.org/protobuf v1.33.0
--- internal/common/testutils/trace.go
+++ internal/common/testutils/trace.go
@@ -1,76 +1,55 @@
 package testutils // import "github.com/joaopgrassi/otel-recipes/internal/common/testutils"
 
 import (
-	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
 	"time"
+
+	otlptrace "go.opentelemetry.io/proto/otlp/trace/v1"
+	"google.golang.org/protobuf/proto"
 )
 
-type JaegerResponse struct {
-	Traces []Trace `json:"data"`
-}
-
-type Trace struct {
-	TraceID string `json:"traceID"`
-	Spans   []Span `json:"spans"`
-}
-
-type Span struct {
-	TraceID       string `json:"traceID"`
-	SpanID        string `json:"spanID"`
-	OperationName string `json:"operationName"`
-	Tags          []Tag  `json:"tags"`
-}
-
-type Tag struct {
-	Key   string      `json:"key"`
-	Value interface{} `json:"value"`
-}
-
-func GetTrace(t *testing.T, serviceName string) *Trace {
-	t.Logf("Going to call Jaeger to fetch trace for sample: %s", serviceName)
-	r, err := http.Get("http://localhost:16686/api/traces?service=" + serviceName)
+func GetTrace(t *testing.T, serviceName string) *otlptrace.ResourceSpans {
+	t.Logf("Going to call OTLP backend to fetch trace for sample: %s", serviceName)
+	r, err := http.Get(fmt.Sprintf("%s/getotlp?signal=trace&serviceName=%s", OtlpBackendUri, serviceName))
 	if err != nil {
-		t.Fatalf("Failed getting trace from Jaeger: %v", err)
+		t.Fatalf("Failed getting trace from OTLP backend: %v", err)
 	}
 
-	t.Log("Received 200 response from Jaeger")
+	t.Log("Received 200 response from OTLP backend")
 
 	defer r.Body.Close()
-	var data JaegerResponse
 
-	err = json.NewDecoder(r.Body).Decode(&data)
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
 	if err != nil {
-		t.Fatalf("Failed decoding json response from Jaeger: %v", err)
+		t.Fatalf("Error reading payload from OTLP backend: %v", err)
 	}
 
-	// useful for CI runs
-	json, _ := json.MarshalIndent(data, "", "  ")
-	t.Logf("Data received from Jaeger: \n%s\n", json)
-
-	if len(data.Traces) == 0 {
-		return nil
+	rs := &otlptrace.ResourceSpans{}
+	err = proto.Unmarshal(body, rs)
+	if err != nil {
+		t.Fatalf("Error reading payload from OTLP backend: %v", err)
 	}
-
-	return &data.Traces[0]
+	return rs
 }
 
-func GetTraceWithRetry(t *testing.T, serviceName string) *Trace {
+func GetTraceWithRetry(t *testing.T, serviceName string) *otlptrace.ResourceSpans {
 	backoffSchedule := []time.Duration{
 		1 * time.Second,
 		3 * time.Second,
 		10 * time.Second,
 	}
 
-	var trace *Trace
+	var rs *otlptrace.ResourceSpans
 
 	// do some retries until we Jaeger has it
 	for _, backoff := range backoffSchedule {
-		trace = GetTrace(t, serviceName)
+		rs = GetTrace(t, serviceName)
 
-		if trace != nil {
+		if rs != nil {
 			break
 		}
 
@@ -79,11 +58,11 @@ func GetTraceWithRetry(t *testing.T, serviceName string) *Trace {
 	}
 
 	// All retries failed
-	if trace == nil {
+	if rs == nil {
 		t.Fatalf("Failed getting trace from Jaeger")
 	}
 
-	return trace
+	return rs
 }
 
 func InvokeSampleApi(t *testing.T, url string) string {
