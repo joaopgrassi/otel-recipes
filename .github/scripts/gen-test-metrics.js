dd3734a487debbdb6d7b93855fe4b4bac5ca664b
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-03-17 22:09:41 +0100

    CI Test

--- .github/scripts/gen-test-metrics.js
+++ .github/scripts/gen-test-metrics.js
@@ -4,17 +4,14 @@ const fs = require("fs");
 function findDockerfileDirectories(modifiedDirs) {
     const dockerfileDirectories = [];
 
-    console.log("test")
-    console.log(modifiedDirs);
-
     modifiedDirs.forEach(dir => {
       let currentPath = dir;
-      const basePath = 'src'; // Most generic base path
+      const basePath = 'src';
 
       while (currentPath !== basePath && currentPath.includes(basePath)) {
         const dockerfileCheckPath = path.join(currentPath, 'Dockerfile');
-        if (fs.existsSync(dockerfileCheckPath)) {
-          dockerfileDirectories.push(currentPath); // Add the directory containing the Dockerfile
+        if (fs.existsSync(dockerfileCheckPath) && !dockerfileDirectories.includes(currentPath)) {
+          dockerfileDirectories.push(currentPath);
           break; // Stop once a Dockerfile is found
         }
         currentPath = path.dirname(currentPath); // Move up a directory level
@@ -23,27 +20,6 @@ function findDockerfileDirectories(modifiedDirs) {
 
     return dockerfileDirectories;
   }
-
-// function findDockerfileDirectories(modifiedFiles) {
-//   const dockerfileDirectories = [];
-
-//   modifiedFiles.forEach((file) => {
-//     let currentPath = path.dirname(file);
-//     const basePath = "src"; // Most generic base path
-
-//     while (currentPath !== basePath && currentPath.includes(basePath)) {
-//       const dockerfileCheckPath = path.join(currentPath, "Dockerfile");
-//       if (fs.existsSync(dockerfileCheckPath)) {
-//         dockerfileDirectories.push(currentPath);
-//         break;
-//       }
-//       // Go up a directory level
-//       currentPath = path.dirname(currentPath);
-//     }
-//   });
-
-//   return dockerfileDirectories;
-// }
 
 const generateBuildMatrix = (modifiedFiles) => {
   return findDockerfileDirectories(JSON.parse(modifiedFiles));
