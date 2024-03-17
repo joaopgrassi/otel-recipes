5594b58e21b48695568d0731377b67bbfb0048c2
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-03-17 18:34:43 +0100

    Add generic CI (#94)

---
+++ .github/scripts/gen-test-metrics.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+
+function findDockerfileDirectories(modifiedDirs) {
+    const dockerfileDirectories = [];
+
+    modifiedDirs.forEach(dir => {
+      let currentPath = dir;
+      const basePath = 'src'; // Most generic base path
+
+      while (currentPath !== basePath && currentPath.includes(basePath)) {
+        const dockerfileCheckPath = path.join(currentPath, 'Dockerfile');
+        if (fs.existsSync(dockerfileCheckPath)) {
+          dockerfileDirectories.push(currentPath); // Add the directory containing the Dockerfile
+          break; // Stop once a Dockerfile is found
+        }
+        currentPath = path.dirname(currentPath); // Move up a directory level
+      }
+    });
+
+    return dockerfileDirectories;
+  }
+
+// function findDockerfileDirectories(modifiedFiles) {
+//   const dockerfileDirectories = [];
+
+//   modifiedFiles.forEach((file) => {
+//     let currentPath = path.dirname(file);
+//     const basePath = "src"; // Most generic base path
+
+//     while (currentPath !== basePath && currentPath.includes(basePath)) {
+//       const dockerfileCheckPath = path.join(currentPath, "Dockerfile");
+//       if (fs.existsSync(dockerfileCheckPath)) {
+//         dockerfileDirectories.push(currentPath);
+//         break;
+//       }
+//       // Go up a directory level
+//       currentPath = path.dirname(currentPath);
+//     }
+//   });
+
+//   return dockerfileDirectories;
+// }
+
+const generateBuildMatrix = (modifiedFiles) => {
+  return findDockerfileDirectories(modifiedFiles);
+};
+
+module.exports = generateBuildMatrix;
