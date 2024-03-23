f6324d61ddea7bbbebee80376f398bbbc00eb077
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-03-23 18:19:06 +0100

    New CI tests for java

---
+++ src/java/trace/console/test/trace_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"testing"
+
+	tu "github.com/joaopgrassi/otel-recipes/internal/common/testutils"
+)
+
+func TestTraceGeneratedFromSample(t *testing.T) {
+	tc := tu.NewSpanTest(
+		tu.WithServiceName("java.console.app"),
+		tu.WithSpanName("HelloWorldSpan"),
+		tu.WithAttributes(tu.StringAttribute("foo", "bar")),
+	)
+
+	tu.AssertSpanWithAttributeExists(t, tc)
+}
