2b21914a90362276862c75f4f8e74acdb3562cdc
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-04-06 12:42:26 +0200

    Add Python console metrics sample

---
+++ src/python/metrics/console/test/metrics_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	tu "github.com/joaopgrassi/otel-recipes/internal/common/testutils"
+)
+
+func TestMetricsGeneratedFromSample(t *testing.T) {
+	rm := tu.GetMetricsWithRetry(t, "python.console.app")
+	m := rm.GetScopeMetrics()[0].Metrics
+
+	// Counter metric
+	ctc := tu.NewMetricTestCase("mycounter", "I count things", "1", int64(3), tu.StringAttribute("foo", "bar"))
+	tu.AssertCounter(t, ctc, m)
+
+	// Gauge metric
+	ctg := tu.NewMetricTestCase("mygauge", "I gauge things", "1", float64(3.5), tu.StringAttribute("foo", "bar"))
+	tu.AssertGauge(t, ctg, m)
+}
