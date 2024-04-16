85e542795edd659175bee4fd138a56cce6a5b469
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-04-16 20:32:22 +0200

    Improve code

--- .github/scripts/validate-dependencies.js
+++ .github/scripts/validate-dependencies.js
@@ -1,4 +1,3 @@
-var glob = require("glob");
 const path = require("path");
 const fs = require("fs");
 const findRecipes = require("./find-recipefiles-to-test");
@@ -7,33 +6,26 @@ function findOutSyncRecipeFiles(modifiedFiles, rootDir) {
   const recipesToTest = findRecipes.findRecipesToTest(modifiedFiles, rootDir);
   let result = [];
   recipesToTest.forEach((f) => {
-    let valid = true;
     const recipePath = path.dirname(f);
     let recipeObj = JSON.parse(fs.readFileSync(f, "utf8"));
-
     switch (recipeObj.languageId) {
       case "csharp":
-        valid = checkCsharpDeps(recipeObj, recipePath);
+        result.push(checkCsharpDeps(recipeObj, recipePath));
         break;
       case "js":
-        valid = checkJsDeps(recipeObj, recipePath);
+        result.push(checkJsDeps(recipeObj, recipePath));
         break;
       case "go":
-        valid = checkGoDeps(recipeObj, recipePath);
+        result.push(checkGoDeps(recipeObj, recipePath));
         break;
       case "java":
-        valid = checkJavaDeps(recipeObj, recipePath);
+        result.push(checkJavaDeps(recipeObj, recipePath));
         break;
       case "python":
-        valid = checkPythonDeps(recipeObj, recipePath);
+        result.push(checkPythonDeps(recipeObj, recipePath));
         break;
       default:
         break;
-    }
-
-    // Save the recipes with out of sync deps
-    if (!valid) {
-      result.push(recipeObj.id);
     }
   });
   return result;
@@ -41,25 +33,19 @@ function findOutSyncRecipeFiles(modifiedFiles, rootDir) {
 
 function checkCsharpDeps(recipe, recipePath) {
   // load the csproj file
-  glob("*.csproj", { cwd: recipePath, nodir: true }, function (er, files) {
-    if (files.length === 0) {
-      console.log("No csproj file found in the directory");
-      return false;
-    }
+  const csprojPath = findFirstFileByExtension(recipePath, ".csproj");
+  if (!csprojPath) {
+    console.log("No csproj file found in the directory");
+    return false;
+  }
 
-    const csproj = fs.readFileSync(path.join(recipePath, files[0]), "utf8");
+  const csproj = fs.readFileSync(csprojPath, "utf8");
 
-    recipe.dependencies.forEach((dep) => {
-      // e.g., <PackageReference Include="OpenTelemetry" Version="1.7.0" />
-      const packageName = `Include="${dep.id}"`;
-      const version = `Version="${dep.version}"`;
-      const found = csproj.includes(packageName) && csproj.includes(version);
-      if (!found) {
-        return false;
-      }
-    });
-    return true;
-  });
+  return checkInvalidDeps(
+    recipe,
+    (dep) => `Include="${dep.id}" Version="${dep.version}"`,
+    (pkgName) => csproj.includes(pkgName)
+  );
 }
 
 function checkJsDeps(recipe, recipePath) {
@@ -68,60 +54,55 @@ function checkJsDeps(recipe, recipePath) {
     "utf8"
   );
 
-  recipe.dependencies.forEach((dep) => {
-    // e.g., "@opentelemetry/api": "^1.8.0"
-    const packageName = `"${dep.id}": "${dep.version}"`;
-    const found = pkgJson.includes(packageName);
-    if (!found) {
-      return false;
-    }
-  });
-  return true;
+  return checkInvalidDeps(
+    recipe,
+    (dep) => `"${dep.id}": "${dep.version}"`,
+    (pkgName) => pkgJson.includes(pkgName)
+  );
 }
 
 function checkGoDeps(recipe, recipePath) {
   const goMod = fs.readFileSync(path.join(recipePath, "go.mod"), "utf8");
 
-  recipe.dependencies.forEach((dep) => {
-    // e.g., go.opentelemetry.io/otel v1.25.0
-    const packageName = `${dep.id} ${dep.version}`;
-    const found = goMod.includes(packageName);
-    if (!found) {
-      return false;
-    }
-  });
-  return true;
+  return checkInvalidDeps(
+    recipe,
+    (dep) => `${dep.id} ${dep.version}`,
+    (pkgName) => goMod.includes(pkgName)
+  );
 }
 
 function checkJavaDeps(recipe, recipePath) {
-  // load the gradle file
-  glob(
-    "**/build.gradle",
-    { cwd: recipePath, nodir: true },
-    function (er, files) {
-      if (files.length === 0) {
-        console.log("No build.gradle file found in the directory");
-        return false;
-      }
+  // load the build.gradle file
+  const gradlePath = findFirstFileByExtension(recipePath, ".gradle");
+  if (!gradlePath) {
+    console.log("No build.gradle file found in the directory");
+    return false;
+  }
 
-      const gradle = fs.readFileSync(path.join(recipePath, files[0]), "utf8");
+  const gradle = fs.readFileSync(gradlePath, "utf8");
 
-      recipe.dependencies.forEach((dep) => {
-        let found = false;
-        if (dep.includes("bom")) {
-          // e.g., implementation platform("io.opentelemetry:opentelemetry-bom:1.36.0")
-          found = gradle.includes(`${dep.id}:${dep.version}`);
-        } else {
-          // e.g., implementation platform("io.opentelemetry:opentelemetry-api")
-          found = gradle.includes(dep.id);
-        }
-        if (!found) {
-          return false;
-        }
-      });
-      return true;
+  invalidDeps = [];
+  recipe.dependencies.forEach((dep) => {
+    let found = false;
+    if (dep.id.includes("bom")) {
+      // e.g., implementation platform("io.opentelemetry:opentelemetry-bom:1.36.0")
+      found = gradle.includes(`${dep.id}:${dep.version}`);
+    } else {
+      // e.g., implementation platform("io.opentelemetry:opentelemetry-api")
+      found = gradle.includes(dep.id);
     }
-  );
+    if (!found) {
+      return invalidDeps.push(dep.id);
+    }
+  });
+  if (invalidDeps.length === 0) {
+    return null;
+  }
+
+  return {
+    id: recipe.id,
+    deps: invalidDeps,
+  };
 }
 
 function checkPythonDeps(recipe, recipePath) {
@@ -130,15 +111,58 @@ function checkPythonDeps(recipe, recipePath) {
     "utf8"
   );
 
+  return checkInvalidDeps(
+    recipe,
+    (dep) => `${dep.id}==${dep.version}`,
+    (pkgName) => req.includes(pkgName)
+  );
+}
+
+function checkInvalidDeps(recipe, pkgNameFunc, includePredicate) {
+  invalidDeps = [];
   recipe.dependencies.forEach((dep) => {
-    // e.g., opentelemetry-api==1.24.0
-    const packageName = `${dep.id}==${dep.version}`;
-    const found = pkgJson.includes(packageName);
+    const packageName = pkgNameFunc(dep);
+    const found = includePredicate(packageName);
     if (!found) {
-      return false;
+      invalidDeps.push(dep.id);
     }
   });
-  return true;
+  if (invalidDeps.length === 0) {
+    return null;
+  }
+
+  return {
+    id: recipe.id,
+    deps: invalidDeps,
+  };
+}
+
+/**
+ * Recursively searches for a file with a specific extension starting from a given directory.
+ *
+ * @param {string} directory The directory to start the search in.
+ * @param {string} extension The desired file extension (including the dot, e.g., '.txt').
+ * @return {string|null} The path to the first file found with the given extension, or null if no file is found.
+ */
+function findFirstFileByExtension(directory, extension) {
+  let entries = fs.readdirSync(directory, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let fullPath = path.join(directory, entry.name);
+    if (entry.isDirectory()) {
+      // Recurse into subdirectory
+      let result = findFirstFileByExtension(fullPath, extension);
+      if (result) {
+        return result;
+      }
+    } else {
+      if (path.extname(entry.name) === extension) {
+        return fullPath;
+      }
+    }
+  }
+
+  return null;
 }
 
 module.exports = findOutSyncRecipeFiles;
