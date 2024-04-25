e37e2a446ee9268653b9c724502bfe2290232832
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-04-25 23:15:41 +0200

    Prepare for build static site

--- src/site/svelte.config.js
+++ src/site/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
@@ -16,7 +16,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	}
 };
 
